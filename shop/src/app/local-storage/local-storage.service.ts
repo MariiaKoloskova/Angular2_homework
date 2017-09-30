3ed913ffe3d574eaf59fa7137904fb685c28d8ab
Mariia Koloskova <Mariia_Koloskova@epam.com>
2017-09-30 15:53:15 +0300

    done with point 2

--- shop/src/app/local-storage/local-storage.service.ts
+++ shop/src/app/local-storage/local-storage.service.ts
@@ -5,4 +5,18 @@ export class LocalStorageService {
 
   constructor() { }
 
+  setItem(key:string, value:string)
+  {
+    localStorage.setItem[key]=value;
+  }
+
+  getItem(key:string): string
+  {
+    return localStorage.getItem(key);
+  }
+
+  removeItem(key:string)
+  {
+    localStorage.removeItem(key);
+  }
 }
